a37154650dc20576875c22b84b159e707985d4df
andruraj <andrewraj10@gmail.com>
2019-07-11 11:22:02 +0530

    Updates

---
+++ precache-manifest.2c6a7bc6fcf3099976e51d6490a7daa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8539324d2f9803e298a7489255823c7e",
+    "url": "/Text-Generator/index.html"
+  },
+  {
+    "revision": "55f3793198f6dbe173f3",
+    "url": "/Text-Generator/static/css/main.d12b890c.chunk.css"
+  },
+  {
+    "revision": "73d4bd57d2948c9e60cd",
+    "url": "/Text-Generator/static/js/2.b2ca986f.chunk.js"
+  },
+  {
+    "revision": "55f3793198f6dbe173f3",
+    "url": "/Text-Generator/static/js/main.ad14b164.chunk.js"
+  },
+  {
+    "revision": "ba31fa95dba9f7f576cf",
+    "url": "/Text-Generator/static/js/runtime~main.ed3aa629.js"
+  }
+]);
